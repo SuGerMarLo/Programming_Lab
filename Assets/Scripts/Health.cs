b622afd63c9d6cb435161de8a8d255a477c4d4dd
SuGerMarLo <116387786+SuGerMarLo@users.noreply.github.com>
2023-03-17 19:44:41 -0400

    Not working

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
     public int maxHealth = 100;
-    public int currentHealth;
+
+    private int currentHealth;
+
+    public TextMeshProUGUI health;
+    public int CurrentHealth
+    {
+        get
+        {
+            return currentHealth;
+        }
+
+        set
+        {
+            currentHealth = value;
+        }
+    }
 
     public int damage;
     public int armor;
@@ -15,19 +31,20 @@ public class Health : MonoBehaviour
     private void Awake()
     {
         currentHealth = maxHealth;
+        StartCoroutine(HealthIncrease());
+        health.text = "Health: " + maxHealth.ToString();
     }
 
     public virtual void TakeDamage(int damage)
     {
         damage -= armor;
-
         damage = Mathf.Clamp(damage, 0, int.MaxValue);
-
         currentHealth -= damage;
 
         if (OnHealthChanged != null)
         {
             OnHealthChanged(maxHealth, currentHealth);
+            health.text = "Health: " + currentHealth.ToString();
         }
 
         if (currentHealth <= 0)
@@ -36,8 +53,43 @@ public class Health : MonoBehaviour
         }
     }
 
+    public virtual void RestoreHealth(int restore)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + restore, 0, maxHealth);
+
+        Debug.Log(currentHealth);
+
+        if (OnHealthChanged != null)
+        {
+            OnHealthChanged(maxHealth, currentHealth);
+            health.text = "Health: " + currentHealth.ToString();
+        }
+    }
+
+    IEnumerator HealthIncrease()
+    {
+        for (int x = 1; x <= maxHealth; x++)
+        {
+            currentHealth = x;
+            if (OnHealthChanged != null)
+            {
+                OnHealthChanged(maxHealth, currentHealth);
+            }
+
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
     public virtual void Die()
     {
         Debug.Log(transform.name + " died!");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(10);
+        }
     }
 }
--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Stats : Health
 {
+
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Enemy")
